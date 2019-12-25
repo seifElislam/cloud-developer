6d821194becef8780855638602f530f8719aff9a
seifElislam <seif.eleslam1990@gmail.com>
2019-12-25 10:32:21 +0200

    Use cors pkg

--- udagram-restapi-feed/src/server.ts
+++ udagram-restapi-feed/src/server.ts
@@ -8,6 +8,8 @@ import bodyParser from 'body-parser';
 import { V0MODELS } from './controllers/v0/model.index';
 import { config } from './config/config';
 
+import cors from "cors";
+
 const c = config.dev;
 
 (async () => {
@@ -20,11 +22,12 @@ const c = config.dev;
   app.use(bodyParser.json());
 
   //CORS Should be restricted
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-  });
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  //   next();
+  // });
+  app.use(cors())
 
   app.use('/api/v0/', IndexRouter)
 
